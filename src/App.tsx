c94f3ec8a1d2596df8d739639f1a446bc3be4c17
Yoan Estrada Blanco <yoanestrada0102@gmail.com>
2024-09-20 09:57:43 -0400

    feat: adding interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { Button } from "./components/ui/Button/Button";
 
 function App() {
+  const [btnColor, setBtnColor] = useState<string>("btn-primary");
+
+  const handleClick = () => {
+    console.log("hola");
+    setBtnColor("btn-success");
+  };
+
   return (
     <div>
-      <Button className="text-white btn-success">add product</Button>
+      <Button className={`text-white ${btnColor}`} onClick={handleClick}>
+        add product
+      </Button>
     </div>
   );
 }
